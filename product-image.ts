4b204ffaffb164392df32954769d52b6bacf09e1
Steve Montgomery <steve@chums.com>
2022-07-06 13:55:15 -0600

    feat(product-images): Add ProductAltItem type, allow null for Sage Item attributes.

--- product-image.ts
+++ product-image.ts
@@ -24,14 +24,26 @@ export interface ProductImage {
     notes: string,
     item_code?: string,
     timestamp:string,
-    ItemCodeDesc?: string,
-    InactiveItem?:string,
-    ProductType?: string,
-    ProductLine?: string,
+    ItemCode?: string|null,
+    ItemCodeDesc?: string|null,
+    InactiveItem?:string|null,
+    ProductType?: string|null,
+    ProductLine?: string|null,
     Category1?:string|null,
     Category?:string|null,
     ItemCollection?: string|null,
-    BaseSKU?:string,
+    BaseSKU?:string|null,
     item_codes?: string[],
     preferred_image?: boolean,
+}
+
+export interface ProductAltItem {
+    id: number,
+    filename: string,
+    item_code: string,
+    itemCode: string,
+    active: boolean,
+    ItemCodeDesc: string
+    ProductType: string,
+    InactiveItem: string
 }
