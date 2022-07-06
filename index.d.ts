194dbdee333ef012353844d81d415e9e70d9fd81
Steve Montgomery <steve@chums.com>
2022-07-06 12:42:57 -0600

    feat: export types in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,10 @@
  */
 
 export * from './basic-types';
-export * from './products';
 export * from './direct-labor';
+export * from './generics';
+export * from './product-image';
+export * from './products';
+export * from './redux';
+export * from './ui/tabs';
+export * from './user';
