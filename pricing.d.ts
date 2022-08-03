609c16fe35865931bc10742779541a6a4e36d884
Steve Montgomery <steve@chums.com>
2022-08-03 10:15:57 -0600

    feat(pricing): refinements for actual usage.

--- pricing.d.ts
+++ pricing.d.ts
@@ -1,27 +1,69 @@
+
 export interface PriceLevel {
     PriceLevel: string;
     PriceLevelDescription: string;
     SortOrder: number;
 }
 
-export interface PriceCode {
+export interface BasePriceCode {
     Company: string;
-    PriceCodeRecord: '0';
+    PriceCodeRecord: '0'|'1'|'2';
     PriceCode: string;
     PriceCodeDesc: string;
     BreakQuantity1: number,
     DiscountMarkup1: number,
     PricingMethod: 'C'|'D'|'M'|'O'|'P',
+    DateUpdated: string;
 }
 
-export interface PriceCodeInfo extends PriceCode {
+export interface FullPriceCode extends BasePriceCode {
+    CustomerPriceLevel: string,
+    ItemCode: string;
+    ARDivisionNo: string;
+    CustomerNo: string;
+    BreakQuantity1: number;
+    BreakQuantity2: number;
+    BreakQuantity3: number;
+    BreakQuantity4: number;
+    BreakQuantity5: number;
+    DiscountMarkup1: number;
+    DiscountMarkup2: number;
+    DiscountMarkup3: number;
+    DiscountMarkup4: number;
+    DiscountMarkup5: number;
+    DateCreated: string;
+    UserCreatedKey: number;
+    DateUpdated: string;
+    UserUpdatedKey: string;
+}
+
+export interface BasePriceCodeInfo extends BasePriceCode {
     ItemsCount: number;
 }
 
-export interface PriceCodeChange extends PriceCode {
+export interface CustomerPriceLevelPriceCode extends BasePriceCode {
+    PriceCodeRecord: '0';
     CustomerPriceLevel: string,
-    oldDiscountMarkup1?: number,
+}
+
+export interface ItemPriceCode extends BasePriceCode {
+    PriceCodeRecord: '1';
+    ItemCode: string;
+}
+
+export interface CustomerPriceCode extends BasePriceCode {
+    PriceCodeRecord: '2';
+    ItemCode: string;
+    ARDivisionNo: string;
+    CustomerNo: string;
+}
+
+
+export interface PriceCodeChange extends BasePriceCode {
+    CustomerPriceLevel: string,
+    newDiscountMarkup1?: number|null,
     UserName: string,
+    timestamp: string;
 }
 
 export interface PriceCodeUsers {
