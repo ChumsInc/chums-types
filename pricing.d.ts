14c11ac35844552b7789ebb36ca995b40c0ad471
Steve Montgomery <steve@chums.com>
2022-08-04 09:56:02 -0600

    fix: not trying namespaces.

--- pricing.d.ts
+++ pricing.d.ts
@@ -1,86 +1,83 @@
 import {DBCompany} from "./basic-types";
 
-export declare namespace Pricing {
-    export interface PriceLevel {
-        PriceLevel: string;
-        PriceLevelDescription: string;
-        SortOrder: number;
-    }
+export interface PriceLevel {
+    PriceLevel: string;
+    PriceLevelDescription: string;
+    SortOrder: number;
+}
 
-    export interface BasePriceCode {
-        Company: DBCompany;
-        PriceCodeRecord: '0'|'1'|'2';
-        PriceCode: string;
-        PriceCodeDesc: string;
-        BreakQuantity1: number,
-        DiscountMarkup1: number,
-        PricingMethod: 'C'|'D'|'M'|'O'|'P',
-        DateUpdated: string;
-    }
+export interface BasePriceCode {
+    Company: DBCompany;
+    PriceCodeRecord: '0'|'1'|'2';
+    PriceCode: string;
+    PriceCodeDesc: string;
+    BreakQuantity1: number,
+    DiscountMarkup1: number,
+    PricingMethod: 'C'|'D'|'M'|'O'|'P',
+    DateUpdated: string;
+}
 
-    export interface FullPriceCode extends BasePriceCode {
-        CustomerPriceLevel: string,
-        ItemCode: string;
-        ARDivisionNo: string;
-        CustomerNo: string;
-        BreakQuantity1: number;
-        BreakQuantity2: number;
-        BreakQuantity3: number;
-        BreakQuantity4: number;
-        BreakQuantity5: number;
-        DiscountMarkup1: number;
-        DiscountMarkup2: number;
-        DiscountMarkup3: number;
-        DiscountMarkup4: number;
-        DiscountMarkup5: number;
-        DateCreated: string;
-        UserCreatedKey: number;
-        DateUpdated: string;
-        UserUpdatedKey: string;
-    }
+export interface FullPriceCode extends BasePriceCode {
+    CustomerPriceLevel: string,
+    ItemCode: string;
+    ARDivisionNo: string;
+    CustomerNo: string;
+    BreakQuantity1: number;
+    BreakQuantity2: number;
+    BreakQuantity3: number;
+    BreakQuantity4: number;
+    BreakQuantity5: number;
+    DiscountMarkup1: number;
+    DiscountMarkup2: number;
+    DiscountMarkup3: number;
+    DiscountMarkup4: number;
+    DiscountMarkup5: number;
+    DateCreated: string;
+    UserCreatedKey: number;
+    DateUpdated: string;
+    UserUpdatedKey: string;
+}
 
-    export interface BasePriceCodeInfo extends BasePriceCode {
-        ItemsCount: number;
-    }
+export interface BasePriceCodeInfo extends BasePriceCode {
+    ItemsCount: number;
+}
 
-    export interface CustomerPriceLevelPriceCode extends BasePriceCode {
-        PriceCodeRecord: '0';
-        CustomerPriceLevel: string,
-    }
+export interface CustomerPriceLevelPriceCode extends BasePriceCode {
+    PriceCodeRecord: '0';
+    CustomerPriceLevel: string,
+}
 
-    export interface ItemPriceCode extends BasePriceCode {
-        PriceCodeRecord: '1';
-        ItemCode: string;
-    }
+export interface ItemPriceCode extends BasePriceCode {
+    PriceCodeRecord: '1';
+    ItemCode: string;
+}
 
-    export interface CustomerPriceCode extends BasePriceCode {
-        PriceCodeRecord: '2';
-        ItemCode: string;
-        ARDivisionNo: string;
-        CustomerNo: string;
-    }
+export interface CustomerPriceCode extends BasePriceCode {
+    PriceCodeRecord: '2';
+    ItemCode: string;
+    ARDivisionNo: string;
+    CustomerNo: string;
+}
 
 
-    export interface PriceCodeChange extends BasePriceCode {
-        CustomerPriceLevel: string,
-        newDiscountMarkup1?: number,
-        UserName: string,
-        timestamp: string;
-    }
+export interface PriceCodeChange extends BasePriceCode {
+    CustomerPriceLevel: string,
+    newDiscountMarkup1?: number,
+    UserName: string,
+    timestamp: string;
+}
 
-    export interface PriceCodeUser {
-        UserName: string;
-        changes: number;
-    }
+export interface PriceCodeUser {
+    UserName: string;
+    changes: number;
+}
 
-    export interface PriceCodeItem {
-        ItemCode: string;
-        ItemCodeDesc: string;
-        ProductType: string;
-        StandardUnitOfMeasure: string;
-        StandardUnitPrice: number;
-        SuggestedRetailPrice: number;
-        AverageUnitCost: number;
-    }
-
+export interface PriceCodeItem {
+    ItemCode: string;
+    ItemCodeDesc: string;
+    ProductType: string;
+    StandardUnitOfMeasure: string;
+    StandardUnitPrice: number;
+    SuggestedRetailPrice: number;
+    AverageUnitCost: number;
 }
