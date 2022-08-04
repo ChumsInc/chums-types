b125641b03d11148def20a6283918c1fe9ad6069
Steve Montgomery <steve@chums.com>
2022-08-04 09:51:50 -0600

    fix: trying namespaces again.

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * CHUMS common types
+ *  @author Steve Montgomery <https://github.com/UtahGooner>
+ */
+
+export * from './basic-types';
+export * from './direct-labor';
+export * from './generics';
+export * from './pricing';
+export * from './product-image';
+export * from './products';
+export * from './returns';
+export * from './sales-orders';
+export * from './salesperson';
+export * from './ui/tabs';
+export * from './user';
