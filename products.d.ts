9002d788d28d0b22330573edd367b1c1be8e1d3d
Steve Montgomery <steve@chums.com>
2022-12-16 11:44:01 -0700

    feat(Product): add ProductSearchItem

--- products.d.ts
+++ products.d.ts
@@ -189,3 +189,16 @@ export interface BinLocation {
     SalesUnitOfMeasure?: string,
     BinLocation: string;
 }
+
+
+export interface ProductSearchItem {
+    company: string;
+    ItemCode: string;
+    ItemCodeDesc: string;
+    ProductType: string;
+    ProductLine:string;
+    Category1: string|null;
+    UDF_UPC: string|null;
+    UDF_UPC_BY_COLOR: string|null;
+    SuggestedRetailPrice: string|number;
+}
