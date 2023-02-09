46ff252a2d834e345fc9939b6e7e71e91fd311a1
Steve Montgomery <steve@chums.com>
2023-02-09 09:08:25 -0700

    fix:user types, export declare enum instead of just export enum...

--- src/user.d.ts
+++ src/user.d.ts
@@ -1,7 +1,7 @@
 import {BooleanLike} from "./basic-types";
 
 
-export enum UserAccountType {
+export declare enum UserAccountType {
     UserAccountNone = 0,
     UserAccountEmployee = 1,
     UserAccountRep = 2,
