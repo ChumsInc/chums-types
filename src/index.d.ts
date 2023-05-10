417d8d1e2bd1ba2e915df7814995ef6b154a2d41
Steve Montgomery <steve@chums.com>
2023-05-10 08:19:07 -0600

    fix: making index.d.ts much simpler.

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,161 +3,21 @@
  *  @author Steve Montgomery <https://github.com/UtahGooner>
  */
 
-export type {FBAItem, FBAItemMap} from './amazon-fba';
-export type {BarcodeItem, BarcodeCustomer, BarcodeCustomerSettings} from './barcode';
-export type {BooleanLike, DBCompany, SageCompany} from './basic-types';
-export type {DLCode, DLCodeStep, DLStep, StepTiming} from './direct-labor';
-export type {Editable, YesNo, LoadingStatus} from './generics';
-export type {Item, WarehouseItem} from './item';
-export type {
-    BasePriceCodeInfo,
-    CustomerPriceCode,
-    CustomerPriceLevelPriceCode,
-    FullPriceCode,
-    ItemPriceCode,
-    PriceCodeChange,
-    PriceLevel,
-    PricingMethod,
-    PriceCodeUser,
-    PriceCodeItem,
-    BasePriceCode
-} from './pricing';
-export type {
-    ProductImage,
-    GenericImage,
-    ImageSize,
-    ImageSizeList,
-    ImageFormatList,
-    ImageSizePath,
-    ColorSpaceList,
-    ProductAltItemKey,
-    ProductAltItem
-} from './product-image';
-export type {
-    ProductType,
-    ProductColor,
-    ProductColorUPCResponse,
-    ProductCategory,
-    ProductLine,
-    ProductColorUPC,
-    ProductSeason,
-    ProductStatus,
-    ProductMixInfo,
-    ProductCollection,
-    ProductMaster,
-    ProductMaterial,
-    ProductDimension,
-    ProductWarehouse,
-    ProductAttributes,
-    BaseSKU,
-    SKUGroup,
-    ProductSearchItem,
-    Warehouse,
-    ProductStatusAttributes,
-    BaseSKUSearch,
-    AdjustableDimension,
-    PrimaryVendor,
-    BinLocation,
-    CountryOfOrigin,
-} from './products';
-export type {
-    RMASalesOrder, RMAOrderStatus, RMAUser, RMAStatus, StatusEvent, StatusKey
-} from './returns';
-export type {
-    SalesOrder, SalesOrderHeader, SalesOrderType, SalesOrderItemType, SalesOrderStatus, SalesOrderDetailLine
-} from './sales-orders';
-export type {Salesperson, SalespersonLookupResult} from './salesperson';
-export type {SortProps} from './sort';
-export type {StoreMapCustomer} from './store-map';
-export {Tab} from './ui/tabs';
-export {
-    User,
-    UserRole,
-    AccessRole,
-    UserAccountType,
-    UserAccount,
-    CustomerUser,
-    UserProfile,
-    ExtendedUser,
-    BasicCustomerUser,
-    ExtendedUserProfile,
-    APIUserProfile,
-    SessionUser,
-    UserRecord,
-    LoadSessionUserProps,
-    TimeClockEmployee,
-    PasswordValidationResponse
-} from './user';
-
+export * from './amazon-fba';
+export * from './barcode';
+export * from './basic-types';
+export * from './direct-labor';
+export * from './generics';
+export * from './item';
+export * from './pricing';
+export * from './product-image';
+export * from './returns';
+export * from './sales-orders';
+export * from './salesperson';
+export * from './shopify/index'
+export * from './sort';
+export * from './sps-types'
+export * from './store-map';
+export * from './ui/tabs';
+export * from './user';
 export * as ShopifyIntegration from './shopify/index'
-export {
-    ShopifyFulfillmentOrderLineItem,
-    ShopifyFulfillmentOrdersList,
-    ShopifyProduct,
-    ShopifyProductVariant,
-    ShopifyProductsResponse,
-    ShopifyOrdersResponse,
-    ShopifyFulfillmentOrder,
-    ShopifyPaymentTransaction,
-    ShopifyPaymentResponse,
-    ShopifyPaymentTransactionsResponse,
-    SavedPayment,
-    ChangedVariant,
-    ShopifyPayment,
-    ShopifyTaxLine,
-    ShopifyShippingLine,
-    ShopifyItem,
-    ShopifyDiscountCode,
-    ItemLocation,
-    ShopifyRisk,
-    ShopifyRiskResponse,
-    ShopifyOrderResponse,
-    ShopifyOrder,
-    ShopifyAddress,
-    ShopifyDiscountAllocation,
-    SavedOrder,
-    SavedOrderRow,
-    ExtendedSavedOrder,
-    SagePaymentType,
-    SageAddress,
-    ExtendedSavedOrderRow,
-    SageImportResponse,
-    SageImportableOrder,
-    ShopifyInventoryLevelResponse,
-    ShopifyInventoryLevel,
-    ShopifyInventoryItemsResponse,
-    SavedInventoryItem,
-    SavedItemUpdate,
-    ShopifyInventoryItem,
-    SavedInventoryLevel,
-    InventoryLevelChange,
-    DeprecatedShopifyFulfillmentBody,
-    FulfillmentResponse,
-    Fulfillment,
-    SavedVariant,
-    SavedProduct,
-    SageOrderDetailLine,
-    SageItem,
-    SageTrackingWithId,
-    SageTrackingResponse,
-    SageFulfillmentTracking,
-    SageFulfillmentTrackingRecord,
-    SageFulfillmentInfo,
-    SageFulfillmentItem,
-    SageFulfillmentItemRecord,
-    ItemValidation,
-    StoreNames,
-    StoreName,
-    ShopifyObject,
-    ShopifyListResult,
-    PriceSet,
-    PriceMoney,
-    MoneyString
-} from './shopify/index'
-
-export {
-    SPSOrderLine, SPSSalesOrder, SPSBaseCustomer, SPSCustomerMap, SPSValueMap, SPSSalesOrderDetailLine,
-    SPSCustomerShipToAddress, SPSCustomerKey, SPSItemUnit, SPSCustomerMapRow, SPSCustomerValueOptions,
-    SPSCustomerOptions, SPSCustomerMapField, SPSCustomerBillingAddress, SPSValueMapRow,
-    SPSConversionResponse, SPSShipToKey
-} from './sps-types'
