a0fd11fd3c4a205781cec996a57a2cc39577b2c0
Steve Montgomery <steve@chums.com>
2023-05-10 07:54:23 -0600

    fix: remove extra fields from Customer Value Map

--- src/sps-types.d.ts
+++ src/sps-types.d.ts
@@ -5,8 +5,9 @@ export interface SPSBaseCustomer {
 }
 
 export interface SPSCustomerKey extends SPSBaseCustomer {
-    Company:string;
+    Company: string;
 }
+
 export interface SPSSalesOrder extends SPSCustomerKey {
     CustomerPONo: string;
     ShipExpireDate: string;
@@ -14,11 +15,11 @@ export interface SPSSalesOrder extends SPSCustomerKey {
     ShipToCode: string;
     WarehouseCode: string;
     zeroCommissions?: boolean;
-    BillToAddress?: SPSCustomerBillingAddress|null;
+    BillToAddress?: SPSCustomerBillingAddress | null;
     DropShip?: boolean;
-    ShipToAddress?:SPSCustomerShipToAddress|null;
-    CarrierCode?: string|null;
-    ShipVia?: string|null;
+    ShipToAddress?: SPSCustomerShipToAddress | null;
+    CarrierCode?: string | null;
+    ShipVia?: string | null;
     detail: SPSSalesOrderDetailLine[];
     comments?: string[];
     SalespersonDivisionNo?: string;
@@ -28,21 +29,22 @@ export interface SPSSalesOrder extends SPSCustomerKey {
 export interface SPSSalesOrderDetailLine {
     _index: string;
     VendorStyle: string;
-    ItemCode: string|null;
-    ItemCodeDesc: string|null;
+    ItemCode: string | null;
+    ItemCodeDesc: string | null;
     QuantityOrdered: number;
     UnitOfMeasure: string;
     UnitPrice: number;
     CommentText: string;
-    UDF_SHIP_CODE: string|null;
+    UDF_SHIP_CODE: string | null;
     errors: string[];
-    csv?:SPSOrderLine;
-    map?: SPSCustomerValueMap;
+    csv?: SPSOrderLine;
+    map?: SPSValueMap;
 }
 
 export interface SPSOrderLine {
     _index: string;
-    [key:string]: string;
+
+    [key: string]: string;
 }
 
 export interface SPSCustomerMapField {
@@ -58,81 +60,81 @@ export interface SPSCustomerMap extends SPSCustomerKey {
     id: number;
     CustomerName: string;
     LookupFields: SPSCustomerMapField[]
-    LookupValue: unknown[]|null;
+    LookupValue: unknown[] | null;
     options: SPSCustomerOptions;
 }
 
 export interface SPSCustomerMapRow
-    extends Omit<SPSCustomerMap, 'LookupFields'|'LookupValue'|'options'> {
-    LookupFields: string|null;
-    LookupValue: string|null;
-    options: string|null;
+    extends Omit<SPSCustomerMap, 'LookupFields' | 'LookupValue' | 'options'> {
+    LookupFields: string | null;
+    LookupValue: string | null;
+    options: string | null;
 }
 
 export interface SPSCustomerValueOptions {
     add?: number;
     conversionFactor?: number;
-    UOMOverride?:string;
+    UOMOverride?: string;
 
 }
-export interface SPSCustomerValueMap extends SPSCustomerKey{
+export interface SPSValueMap {
     id: number;
-    customerId: number;
-    MapField: 'ShipExpireDate'|'CancelDate'|'ShipToCode'|'ShipVia'|'ItemCode';
-    CSVField:string;
-    CustomerValue:string;
-    MappedValue: string|null;
-    MappedOptions: SPSCustomerValueOptions|null;
+    MapField: 'ShipExpireDate' | 'CancelDate' | 'ShipToCode' | 'ShipVia' | 'ItemCode';
+    CSVField: string;
+    CustomerValue: string;
+    MappedValue: string | null;
+    MappedOptions: SPSCustomerValueOptions | null;
 }
 
-export interface SPSCustomerValueMapRow
-    extends Omit<SPSCustomerValueMap,  'MappedOptions'> {
+export interface SPSValueMapRow
+    extends Omit<SPSValueMap, 'MappedOptions'> {
 }
 
 export interface SPSItemUnit {
     ItemCode: string;
-    ItemCodeDesc:string;
+    ItemCodeDesc: string;
     SalesUnitOfMeasure: string;
     SalesUMConvFctr: number;
     StandardUnitOfMeasure: string;
-    InactiveItem: 'Y'|'N';
-    ProductType: 'D'|'F'|'K'|'R';
+    InactiveItem: 'Y' | 'N';
+    ProductType: 'D' | 'F' | 'K' | 'R';
     BillType: string;
 }
 
 export interface SPSCustomerBillingAddress {
-    CustomerName:string;
-    AddressLine1:string|null;
-    AddressLine2:string|null;
-    AddressLine3:string|null;
-    City:string;
-    State:string;
-    ZipCode:string;
-    CountryCode:string;
+    CustomerName: string;
+    AddressLine1: string | null;
+    AddressLine2: string | null;
+    AddressLine3: string | null;
+    City: string;
+    State: string;
+    ZipCode: string;
+    CountryCode: string;
 }
+
 export interface SPSShipToKey extends SPSCustomerKey {
     ShipToCode: string;
 }
 
 export interface SPSCustomerShipToAddress {
     ShipToCode: string;
-    ShipToName:string;
-    ShipToAddress1:string|null;
-    ShipToAddress2:string|null;
-    ShipToAddress3:string|null;
-    ShipToCity:string;
-    ShipToState:string;
-    ShipToZipCode:string;
-    ShipToCountryCode:string;
-    WarehouseCode: string|null;
+    ShipToName: string;
+    ShipToAddress1: string | null;
+    ShipToAddress2: string | null;
+    ShipToAddress3: string | null;
+    ShipToCity: string;
+    ShipToState: string;
+    ShipToZipCode: string;
+    ShipToCountryCode: string;
+    WarehouseCode: string | null;
 }
 
 
 export interface SPSConversionResponse {
     SalesOrder: SPSSalesOrder;
-    mapping: SPSCustomerValueMap[];
+    mapping: SPSValueMap[];
     customer: SPSCustomerMap | null;
     unitsOfMeasure: SPSItemUnit[];
-    ItemCodes: (string|null)[];
+    ItemCodes: (string | null)[];
     csvLines?: SPSOrderLine[];
 }
