095c8bb123b6a37f0421ac8802e94e0497ec08ec
Steve Montgomery <steve@chums.com>
2025-01-02 16:28:23 -0700

    add MediaConnection to ProductVariantNode

--- src/shopify/graphql/variant.d.ts
+++ src/shopify/graphql/variant.d.ts
@@ -1,19 +1,20 @@
 import {ProductNode} from "./product.js";
 import {Edge, UserError} from "./generics.js";
 import {InventoryItem} from "./inventory.js";
+import {MediaConnection} from "./media.js";
 
 export interface VariantsConnection<T> {
     edges: Edge<T>[];
 }
 
-export type ProductVariantsConnection = VariantsConnection<ProductVariant>;
+export type ProductVariantsConnection = VariantsConnection<ProductVariantNode>;
 
 export interface SelectedOption {
     name: string;
     value: string;
 }
 
-export interface ProductVariant {
+export interface ProductVariantNode {
     id: string;
     product: Pick<ProductNode, 'id'>;
     legacyResourceId: string;
@@ -22,13 +23,30 @@ export interface ProductVariant {
     compareAtPrice: string;
     sku: string;
     barcode: string;
+    media: MediaConnection;
     selectedOptions: SelectedOption[];
     inventoryItem: Pick<InventoryItem, 'id' | 'measurement' | 'provinceCodeOfOrigin' | 'harmonizedSystemCode' | 'inventoryLevel'>;
     createdAt: string;
     updatedAt: string;
     status?: string;
 }
-export type ProductVariantNode = ProductVariant;
+
+export interface ProductVariant extends Omit<ProductVariantNode, 'media'> {
+    id: string;
+    product: Pick<ProductNode, 'id'>;
+    legacyResourceId: string;
+    title: string;
+    price: string;
+    compareAtPrice: string;
+    sku: string;
+    barcode: string;
+    media: string[];
+    selectedOptions: SelectedOption[];
+    inventoryItem: Pick<InventoryItem, 'id' | 'measurement' | 'provinceCodeOfOrigin' | 'harmonizedSystemCode' | 'inventoryLevel'>;
+    createdAt: string;
+    updatedAt: string;
+    status?: string;
+}
 
 export interface ProductVariantPriceBody {
     productId: string;
