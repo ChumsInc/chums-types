13133ebdcd17931d31c2d542fde0aefff609de9b
Steve Montgomery <steve@chums.com>
2025-04-08 11:07:50 -0600

    add MailerReportContent & MailerMultiPartContent

--- src/mailer.d.ts
+++ src/mailer.d.ts
@@ -1,7 +1,7 @@
 
-export interface MailerReportObject {
+export interface MailerMultiPartContent {
     html: string;
     textContent: string;
     attachments: unknown[]; // needs to match SendMailProps from chums-local-modules/lib/mailer.ts (see nodemailer/lib/mailer/index.js)
 }
-export type MailerReportContent = MailerReportObject|string;
+export type MailerReportContent = MailerMultiPartContent|string;
