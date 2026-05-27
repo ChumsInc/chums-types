bfdf397a02954b003b74823eea9cf63b1491b7d3
Steve Montgomery <steve@chums.com>
2026-05-27 09:28:32 -0600

    fix: simplify `PageState` interface by removing unused fields

--- src/b2b/state/page-state.d.ts
+++ src/b2b/state/page-state.d.ts
@@ -1,11 +1,6 @@
-import type {Keyword} from "../keyword.d.ts";
 import type {ContentPage} from "../ui-features.d.ts";
 
 export interface PageState {
-    list: Keyword[],
     keyword: string | null;
-    status: 'idle' | 'loading';
-    loaded: boolean;
     content: ContentPage | null;
-    html: string | null;
 }
